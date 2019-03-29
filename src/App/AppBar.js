69405e20cb8076b71554a3742f36f9a70c1fc382
kevin <kevinbalvarez@gmail.com>
2019-03-28 18:07:14 -0600

    First commit

---
+++ src/App/AppBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {AppContext} from './AppProvider';
+
+const logo = styled.div`
+    font-size: 1.5em;
+`
+
+const Bar = styled.div`
+    display: grid;
+    margin-bottom: 40px;
+    grid-template-columns: 180px auto 100px 100px;
+`
+
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    ${props => props.active && css`
+        text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({name}){
+    return (
+        <AppContext.Consumer>
+            {({page, setPage}) => (
+                <ControlButtonElem 
+                    onClick={() => setPage(name)} 
+                    active={page === name}
+                >
+                    {toProperCase(name)}
+                </ControlButtonElem>
+            )}
+        </AppContext.Consumer>
+    )
+}
+
+export default function(){
+    return (
+        <Bar>
+            <div> CryptoDash </div>
+            <div/>
+            <ControlButton active name="dashboard" />
+            <ControlButton name="settings" />
+        </Bar>
+    ) 
+
+}
---
+++ src/App/AppProvider.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export const AppContext = React.createContext();
+
+export class AppProvider extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            page: 'dashboard',
+            setPage: this.setPage,
+            ...this.savedSettings(),
+            confirmFavorites: this.confirmFavorites
+        }
+    }
+
+    setPage = page => this.setState({page})
+
+    savedSettings(){
+        let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+        if(!cryptoDashData){
+            return {page: 'settings', firstVisit: true}
+        }
+        return {}
+    }
+
+    confirmFavorites = () =>{
+        this.setState({
+            firstVisit: false,
+            page: 'dashboard'
+        });
+        localStorage.setItem('cryptoDash', JSON.stringify({
+            test: 'hello'
+        }))
+    }
+    
+
+    render(){
+        return (
+            <AppContext.Provider value={this.state}>
+                {this.props.children}
+            </AppContext.Provider>
+        )
+    }
+}
